315678ce9d1cc11d19fdb3868275d8026840d8d5
Nooran Tarek <102181986+NooranTarek@users.noreply.github.com>
2024-01-27 11:51:11 +0200

    Delete lab3EcommerceApp directory

--- lab3EcommerceApp/src/app/discount-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DiscountPipePipe } from './discount-pipe.pipe';
-
-describe('DiscountPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new DiscountPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
